672d3cea3ef2d9add7c9f156f6c96feacc3a57d7
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-02-26 23:44:53 +0300

    Minnor edit

--- TrolleybusRoutes/Route/Route.cs
+++ TrolleybusRoutes/Route/Route.cs
@@ -3,12 +3,23 @@
     public class Route
     {
         public int NumberOfRoute { get; set; }
-        public List<Stop> Stops { get; private set; }
+        private List<Stop> _stops;
 
         public Route(int numOfRoute, List<Stop> stops)
         {
             NumberOfRoute = numOfRoute;
-            Stops = stops;
+            _stops = stops;
+        }
+
+        public string GetStops()
+        {
+            string ListOfStops = "";
+            foreach (Stop stop in _stops)
+            {
+                ListOfStops += stop.ToString() + "\n";
+            }
+
+            return ListOfStops;
         }
     }
 }
--- TrolleybusRoutes/Route/Stop.cs
+++ TrolleybusRoutes/Route/Stop.cs
@@ -2,13 +2,31 @@
 {
     public class Stop
     {
-        public string Name { get; private set; }
-        public StopSchedule Schedule { get; private set; }
+        public string Name { get; set; }
+        private StopSchedule _weekdaySchedule;
+        private StopSchedule _weekendsSchedule;
 
-        public Stop(string Name, StopSchedule Schedule)
+
+        public Stop(string Name, StopSchedule WeekdaySchedule, StopSchedule WeekendsSchedule)
         {
             this.Name = Name;
-            this.Schedule = Schedule;
+            _weekdaySchedule = WeekdaySchedule;
+            _weekendsSchedule = WeekendsSchedule;
+        }
+
+        public string GetWeekdaySchedule()
+        {
+            return _weekdaySchedule.ToString();
+        }
+
+        public string GetWeekendsSchedule()
+        {
+            return _weekendsSchedule.ToString();
+        }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
