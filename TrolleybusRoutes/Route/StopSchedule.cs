455d86d83910438e60f7e8bc0c5621023190fa95
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-02-27 01:42:59 +0300

    Minnor edit

--- TrolleybusRoutes/Route/StopSchedule.cs
+++ TrolleybusRoutes/Route/StopSchedule.cs
@@ -3,12 +3,9 @@
     public class StopSchedule
     {
         public List<DateTime> ListOfArriveTime { get; private set; }
-        public List<DateTime> ReverseListOfArriveTime { get; private set; }
         public StopSchedule(List<DateTime> ListOfArriveTime)
         {
             this.ListOfArriveTime = ListOfArriveTime;
-            ReverseListOfArriveTime = this.ListOfArriveTime;
-            ReverseListOfArriveTime.Reverse();
         }
 
     }
